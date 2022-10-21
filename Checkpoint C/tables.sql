8454e12c89370ea0c37afe62a01d9454d8eb8d84
siavava <amittaijoel@outlook.com>
2022-10-21 03:56:44 -0400

    Update: Create all tables and relationships.

--- Checkpoint C/tables.sql
+++ Checkpoint C/tables.sql
@@ -1,127 +1,149 @@
-/* Scripts to create tables in the database */
+/*
+  This file contains the SQL statements
+  to create tables in the database.
+  For the schema, see the schema.pdf file.
 
-CREATE DATABASE ps2;
+  Authors: Amittai Siavava, Ke Lou
+ */
 
-USE ps2;
 
-/* Create Manuscript table */
-DROP TABLE IF EXISTS Manuscript;
-CREATE TABLE Manuscript
-  (
-    manuscript_NO     INT           NOT NULL  PRIMARY KEY   AUTO_INCREMENT,
-    title             VARCHAR(255)  NOT NULL,
-    date_received     DATE          NOT NULL,
-    status            VARCHAR(45),
-    page_count        INT,
-    date_accepted     DATE,
-    RICode_code       INT   FOREIGN KEY   REFERENCES RICode(code),
-    Editor_editor_ID  INT   FOREIGN KEY   REFERENCES Editor(editor_ID),
-    Issue_issue_ID    INT   FOREIGN KEY   REFERENCES Issue(issue_ID)
-  );
+-- First, clear the database.
+-- We turn off foreign-key checks
+-- to allow deletion of referenced tables,
+-- then turn foreign-key checks back on.
+ 
+SET FOREIGN_KEY_CHECKS = 0;
 
-/* Create Issue table */
-DROP TABLE IF EXISTS Issue;
-CREATE TABLE Issue
-  (
-    issue_ID            INT   NOT NULL  PRIMARY KEY AUTO_INCREMENT,
-    year                INT   NOT NULL,
-    period              INT   NOT NULL,
-    publication_date    DATE,
-    Journal_journal_ID  INT   FOREIGN KEY   REFERENCES Journal(journal_ID)
-  );
-
-/* Create Author table */
-DROP TABLE IF EXISTS Author;
-CREATE TABLE Author
-  (
-    author_ID                   INT   NOT NULL  PRIMARY KEY   AUTO_INCREMENT,
-    Affiliation_affiliation_ID  INT   NOT NULL  FOREIGN KEY   REFERENCES Affiliation(affiliation_ID),
-    f_name                      VARCHAR(45),
-    l_name                      VARCHAR(45),
-    email                       VARCHAR(45)
-  );
-
-/* Create Affiliation table */
-DROP TABLE IF EXISTS Affiliation;
-CREATE TABLE Affiliation
-  (
-    affiliation_ID  INT   NOT NULL  PRIMARY KEY AUTO_INCREMENT,
-    name            VARCHAR(45)
-  );
-
-/* Create RICode table */
 DROP TABLE IF EXISTS RICode;
+DROP TABLE IF EXISTS Affiliation;
+DROP TABLE IF EXISTS Journal;
+DROP TABLE IF EXISTS Issue;
+DROP TABLE IF EXISTS Editor;
+DROP TABLE IF EXISTS Reviewer;
+DROP TABLE IF EXISTS Author;
+DROP TABLE IF EXISTS Manuscript_Author;
+DROP TABLE IF EXISTS Journal_has_RICodes;
+DROP TABLE IF EXISTS Reviewer_has_Manuscript;
+DROP TABLE IF EXISTS Manuscript;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+
+
+-- Create tables topologically,
+-- starting with those that have no foreign keys.
+
+
 CREATE TABLE RICode
   (
     code      INT   NOT NULL  PRIMARY KEY   AUTO_INCREMENT,
     interest  VARCHAR(45)
   );
 
-/* Create Journal table */
-DROP TABLE IF EXISTS Journal;
+CREATE TABLE Affiliation
+  (
+    affiliation_ID  INT   NOT NULL  PRIMARY KEY AUTO_INCREMENT,
+    name            VARCHAR(45)
+  );
+
 CREATE TABLE Journal
   (
     journal_ID    INT   NOT NULL      PRIMARY KEY AUTO_INCREMENT,
-    RICode_code   INT   FOREIGN KEY   REFERENCES RICode(code),
+    RICode_code   INT,
+    FOREIGN KEY   (RICode_code)       REFERENCES RICode(code)
   );
 
-/* Create Editor table */
-DROP TABLE IF EXISTS Editor;
+CREATE TABLE Issue
+  (
+    issue_ID            INT   NOT NULL  PRIMARY KEY   AUTO_INCREMENT,
+    year                INT   NOT NULL,
+    period              INT   NOT NULL,
+    publication_date    DATE,
+    Journal_journal_ID  INT,
+    FOREIGN KEY         (Journal_journal_ID)  REFERENCES Journal(journal_ID)
+  );
+
 CREATE TABLE Editor
   (
     editor_ID           INT   NOT NULL  PRIMARY KEY   AUTO_INCREMENT,
     email               VARCHAR(45),
     f_name              VARCHAR(45),
     l_name              VARCHAR(45),
-    Journal_journal_ID  INT   FOREIGN KEY   REFERENCES Journal(journal_ID)
+    Journal_journal_ID  INT,
+    FOREIGN KEY         (Journal_journal_ID)   REFERENCES Journal(journal_ID)
   );
 
-/* Create Reviewer table */
-DROP TABLE IF EXISTS Reviewer;
 CREATE TABLE Reviewer
   (
     reviewer_ID                 INT   NOT NULL  PRIMARY KEY   AUTO_INCREMENT,
     email                       VARCHAR(45),
     f_name                      VARCHAR(45),
     l_name                      VARCHAR(45),
-    Affiliation_affiliation_ID  INT   FOREIGN KEY   REFERENCES Affiliation(affiliation_ID)
+    Affiliation_affiliation_ID  INT,
+    FOREIGN KEY   (Affiliation_affiliation_ID)   REFERENCES Affiliation(affiliation_ID)
   );
 
-/*
-  Create helper tables for relationships.
-*/
+CREATE TABLE Author
+  (
+    author_ID                   INT   NOT NULL  PRIMARY KEY   AUTO_INCREMENT,
+    Affiliation_affiliation_ID  INT   NOT NULL,
+    f_name                      VARCHAR(45),
+    l_name                      VARCHAR(45),
+    email                       VARCHAR(45),
+    FOREIGN KEY   (Affiliation_affiliation_ID)   REFERENCES Affiliation(affiliation_ID)
+  );
 
-/* Create Manuscript_Author table */
-DROP TABLE IF EXISTS Manuscript_Author;
+CREATE TABLE Manuscript
+  (
+    manuscript_NO     INT           NOT NULL  PRIMARY KEY   AUTO_INCREMENT,
+    title             VARCHAR(45)   NOT NULL,
+    date_received     DATE          NOT NULL,
+    status            VARCHAR(45),
+    page_count        INT,
+    date_accepted     DATE,
+    RICode_code       INT,
+    Editor_editor_ID  INT,
+    Issue_issue_ID    INT,
+    FOREIGN KEY       (RICode_code)       REFERENCES RICode(code),
+    FOREIGN KEY       (Editor_editor_ID)  REFERENCES Editor(editor_ID),
+    FOREIGN KEY       (Issue_issue_ID)    REFERENCES Issue(issue_ID)
+  );
+
+
+--  Create helper tables for relationships.
+
+
 CREATE TABLE Manuscript_Author
   (
-    Manuscript_manuscript_NO  INT   NOT NULL  FOREIGN KEY   REFERENCES Manuscript(manuscript_NO),
-    Author_author_ID          INT   NOT NULL  FOREIGN KEY   REFERENCES Author(author_ID),
+    Manuscript_manuscript_NO  INT   NOT NULL,
+    Author_author_ID          INT   NOT NULL,
     author_ordinal            INT,
-    PRIMARY KEY               (Manuscript_manuscript_NO, Author_author_ID)
+    PRIMARY KEY   (Manuscript_manuscript_NO, Author_author_ID),
+    FOREIGN KEY   (Manuscript_manuscript_NO)  REFERENCES Manuscript(manuscript_NO),
+    FOREIGN KEY   (Author_author_ID)          REFERENCES Author(author_ID)
   );
 
-/* Create Journal_has_RICodes table */
-DROP TABLE IF EXISTS Journal_has_RICodes;
 CREATE TABLE Journal_has_RICodes
   (
-    Journal_journal_ID  INT   NOT NULL  FOREIGN KEY   REFERENCES Journal(journal_ID),
-    RICode_code         INT   NOT NULL  FOREIGN KEY   REFERENCES RICode(code),
-    PRIMARY KEY         (Journal_journal_ID, RICode_code)
+    Journal_journal_ID  INT   NOT NULL,
+    RICode_code         INT   NOT NULL,
+    PRIMARY KEY         (Journal_journal_ID, RICode_code),
+    FOREIGN KEY   (Journal_journal_ID)  REFERENCES Journal(journal_ID),
+    FOREIGN KEY   (RICode_code)         REFERENCES RICode(code)
   );
 
-/* Create Reviewer_has_Manuscript table */
-DROP TABLE IF EXISTS Reviewer_has_Manuscript;
 CREATE TABLE Reviewer_has_Manuscript
   (
-    Reviewer_reviewer_ID      INT   NOT NULL FOREIGN KEY REFERENCES Reviewer(reviewer_ID),
-    Manuscript_manuscript_NO  INT   NOT NULL FOREIGN KEY REFERENCES Manuscript(manuscript_NO),
+    Reviewer_reviewer_ID      INT   NOT NULL,
+    Manuscript_manuscript_NO  INT   NOT NULL,
     date_sent                 DATE,
     appropriateness           INT   CHECK (appropriateness >= 1 AND appropriateness <= 10),
-    clarity                   INT   CHECK (clarity >= 1 AND clarity <= 10),
-    methodology               INT   CHECK (methodology >= 1 AND methodology <= 10),
-    experimental              INT   CHECK (experimental >= 1 AND experimental <= 10),
-    recommendation            INT   CHECK (recommendation >= 1 AND recommendation <= 10),
+    clarity                   INT   CHECK (clarity >= 1         AND clarity <= 10),
+    methodology               INT   CHECK (methodology >= 1     AND methodology <= 10),
+    experimental              INT   CHECK (experimental >= 1    AND experimental <= 10),
+    recommendation            INT   CHECK (recommendation >= 1  AND recommendation <= 10),
     feedback_date             DATE,
-    PRIMARY KEY               (Reviewer_reviewer_ID, Manuscript_manuscript_NO)
+    PRIMARY KEY               (Reviewer_reviewer_ID, Manuscript_manuscript_NO),
+    FOREIGN KEY   (Reviewer_reviewer_ID)      REFERENCES Reviewer(reviewer_ID),
+    FOREIGN KEY   (Manuscript_manuscript_NO)  REFERENCES Manuscript(manuscript_NO)
   );
