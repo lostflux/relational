ea73efce506c615611a456c88ac8ffcb35e9c7bb
siavava <amittaijoel@outlook.com>
2022-10-23 14:49:50 -0400

    AutoRejectManuscriptOnNoReviewers

--- Checkpoint C/triggersetup.sql
+++ Checkpoint C/triggersetup.sql
@@ -1,6 +1,31 @@
 /* Script for definition of triggers in the database */
 
 /*
+  When an author is submitting a new manuscript to the system
+  with an ICode for which there is no reviewer 
+  raise an exception that informs the author the paper
+  can not be considered at this time.
+ */
+
+DROP TRIGGER IF EXISTS AutoRejectManuscriptOnNoReviewers;
+DELIMITER $$
+CREATE TRIGGER AutoRejectManuscriptOnNoReviewers
+  BEFORE INSERT ON Manuscript
+  FOR EACH ROW
+  BEGIN
+    DECLARE counter INT;
+    SELECT COUNT(*) INTO counter
+    FROM Reviewer_has_RICodes
+    WHERE RICodes_code = NEW.RICodes_code;
+    IF counter = 0 THEN
+      SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Sorry, we cannot review your manuscript at this time.';
+    END IF;
+  END$$
+DELIMITER ;
+
+
+/*
   Trigger: ManuscriptStatusChange
   Purpose: When a manuscript's status is changed, update the status_change_date.
   Permissions: Editor.
