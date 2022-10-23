e0efd831827e432b63000cdd28ff45584d578a01
fpoon777 <fpoon777@gmail.com>
2022-10-23 15:41:23 -0400

    addedd reviewer_has_ricode

--- Checkpoint C/insert.sql
+++ Checkpoint C/insert.sql
@@ -1126,3 +1126,79 @@ VALUES
   (85,23,1),
   (86,9,1),
   (87,50,1);
+
+-- number of reviewer_has_ricode: 60
+INSERT INTO Reviewer_has_RICodes (Reviewer_reviewer_id,RICodes_code)
+VALUES
+  (1,4),
+  (2,8),
+  (3,7),
+  (4,1),
+  (5,3),
+  (6,6),
+  (7,5),
+  (8,2),
+  (9,3),
+  (10,2);
+INSERT INTO Reviewer_has_RICodes (Reviewer_reviewer_id,RICodes_code)
+VALUES
+  (11,3),
+  (12,1),
+  (13,7),
+  (14,10),
+  (15,4),
+  (16,7),
+  (17,8),
+  (18,4),
+  (19,7),
+  (20,4);
+INSERT INTO Reviewer_has_RICodes (Reviewer_reviewer_id,RICodes_code)
+VALUES
+  (21,8),
+  (22,2),
+  (23,3),
+  (24,2),
+  (25,2),
+  (26,7),
+  (27,4),
+  (28,9),
+  (29,6),
+  (30,10);
+INSERT INTO Reviewer_has_RICodes (Reviewer_reviewer_id,RICodes_code)
+VALUES
+  (31,7),
+  (32,3),
+  (33,3),
+  (34,9),
+  (35,8),
+  (36,10),
+  (37,6),
+  (38,1),
+  (39,4),
+  (40,8);
+INSERT INTO Reviewer_has_RICodes (Reviewer_reviewer_id,RICodes_code)
+VALUES
+  (41,9),
+  (42,7),
+  (43,5),
+  (44,3),
+  (45,5),
+  (46,5),
+  (47,5),
+  (48,7),
+  (49,5),
+  (50,8);
+INSERT INTO Reviewer_has_RICodes (Reviewer_reviewer_id,RICodes_code)
+VALUES
+  (51,4),
+  (52,5),
+  (53,6),
+  (54,1),
+  (55,4),
+  (56,3),
+  (57,3),
+  (58,1),
+  (59,3),
+  (60,6);
+
+
